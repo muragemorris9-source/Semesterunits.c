16f44caba9691e376db9337c99e625044154898e
muragemorris9-source <muragemorris9@gmail.com>
2025-09-22 23:50:17 +0300

    Add files via upload

---
+++ semester units.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//simple c programming 
+/*
+NAME: Morris Murage 
+REG NO:CT100/G/26143/25
+DESCRIPTION: Program to compute and display all my semester units 
+*/
+int main() {
+    // First Year semester 1 units
+    printf("1. Analogue electronics\n");
+    printf("2. Communication Skills\n");
+    printf("3. Introduction to computer applications\n");
+    printf("4. Essentials of accounting\n");
+    printf("5. Mathematics for science\n");
+    printf("6. Fundamentals of ICT\n");
+    printf("7. Introduction to programming and algorithms\n");
+    printf("8. Computer systems and organisation\n");
+     return 0;
+}
